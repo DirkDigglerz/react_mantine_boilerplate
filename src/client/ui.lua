de08e3b561dac6e6fefb5d7b21059051a9166ca6
DirkScripts <77217316+DirkDigglerz@users.noreply.github.com>
2024-09-30 01:33:41 +0100

    Update ui.lua
    added custom theme ability

--- src/client/ui.lua
+++ src/client/ui.lua
@@ -2,6 +2,18 @@ RegisterNuiCallback('GET_SETTINGS', function(data, cb)
   cb({
     primaryColor = 'clean', 
     primaryShade = 9, 
+    customTheme  = {
+      "#f8edff",
+      "#e9d9f6",
+      "#d0b2e8",
+      "#b588da",
+      "#9e65cf",
+      "#914ec8",
+      "#8a43c6",
+      "#7734af",
+      "#692d9d",
+      "#5c258b"
+    },
      
     -- ADD YOUR SETTINGS HERE THEY WILL PULL WHEN THE UI INITIALIZES
   })
