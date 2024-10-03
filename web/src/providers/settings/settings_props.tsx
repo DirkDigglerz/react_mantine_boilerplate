c1dd431d629790d14bc274ae2e1048d863941f95
DirkScripts <77217316+DirkDigglerz@users.noreply.github.com>
2024-10-03 05:51:31 +0100

    Update settings_props.tsx

--- web/src/providers/settings/settings_props.tsx
+++ web/src/providers/settings/settings_props.tsx
@@ -3,7 +3,7 @@ import { MantineColor, MantineColorShade } from "@mantine/core";
 export type SettingsProps = {
   primaryColor: MantineColor;
   primaryShade: MantineColorShade;
-  themeColor: MantineColorTuples;
+  themeColor: MantineColorsTuple;
 
   // Add more settings here
 };
